e1b636f61636dae4ddfe683e94f22392afabc609
louishol <louis_hol@hotmail.com>
2015-03-13 16:29:33 +0100

    Api Communicatie

---
+++ www/js/api.js
@@ -0,0 +1,28 @@
+
+function getAllRestaurants()
+{
+	var restaurant =  getRequest("https://api.eet.nu/venues/");
+	return restaurant;
+}
+
+function getRestaurantByID(id)
+{
+	var restaurant =  getRequest("https://api.eet.nu/venues/"+id);
+	return restaurant;
+}
+
+
+function getRequest(newUrl) {
+	var returndata = "ka";
+    $.ajax({
+        url: newUrl,
+        async: false,
+        dataType: 'html',
+        type: "GET",
+        cache: false,
+        success: function (data) {
+            returndata = data;
+        }
+    });
+    return returndata;
+} 
