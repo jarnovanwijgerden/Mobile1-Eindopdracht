27c71f89602196a4f811b64c64ee6cbda0a742d5
louishol <louis_hol@hotmail.com>
2015-03-31 19:46:12 +0200

    test

--- www/js/database.js
+++ www/js/database.js
@@ -11,9 +11,6 @@ function onDeviceReady() {
 
 var db;
 
-
-
-
 function btnSaveClicked(){
     var categorie = $('#category').val();
     var afstand = $('#distance').val();
@@ -22,11 +19,17 @@ function btnSaveClicked(){
 }
 
 function initializeDatabase() {
-    db = window.openDatabase("Database", "1.0", "eet.nu", 2 * 1024 * 1024);
-    db.transaction(function(trans)
-    { 
-        trans.executeSql('CREATE TABLE IF NOT EXISTS settings (categorie TEXT, afstand TEXT, waardering BOOLEAN)');
-    }, errorCB);
+    //db = window.openDatabase("Database", "1.0", "eet.nu", 2 * 1024 * 1024);
+    db = window.sqlitePlugin.openDatabase({name: "eet.nu"});
+    // db.transaction(function(trans)
+    // { 
+    //     trans.executeSql('CREATE TABLE IF NOT EXISTS settings (categorie TEXT, afstand TEXT, waardering BOOLEAN)');
+    // }, errorCB);
+     db.transaction(function(tx) {
+            tx.executeSql('DROP TABLE IF EXISTS settings');
+            tx.executeSql('CREATE TABLE IF NOT EXISTS settings (categorie TEXT, afstand TEXT, waardering BOOLEAN)');
+            alert("DB AANGEMAAKT");
+    });
 }
 
 function errorCB(err) {
@@ -34,36 +37,58 @@ function errorCB(err) {
     return true;
 }
 
-function getSettings(succes, error) {
+function getSettings(callback) {
 
-    var categorie = "";
-    var afstand = "";
-    var waardering = "";
-    alert("ioN DE SETTINGS");
-    db.transaction(function(trans)
-    { 
-        alert("In de dbtans");
-        trans.executeSql('SELECT * FROM settings', [], function(trans, results)
-        {
-            alert("in de select statement");
-            var len = results.rows.length;
+
+    db.transaction(function(tx) {
+            tx.executeSql("select categorie, afstand, waardering from settings;", [], function(tx, res) {
+
+            alert("In de settings");
+            var len = res.rows.length;
             if(len>0)
             {
-                categorie = results.rows.item(0).categorie;
-                afstand = results.rows.item(0).afstand;
-                waardering = results.rows.item(0).waardering;
+                categorie = res.rows.item(0).categorie;
+                afstand = res.rows.item(0).afstand;
+                waardering = res.rows.item(0).waardering;
                 succes(categorie, afstand, waardering);
             }
-        }, 
-        function(err)
-        {
-            error(err);
-        });
-    }, 
-    function(err)
-    {
-         error(err);
-    });
+
+            callback(categorie, afstand, waardering);
+
+            });
+   });
+
+
+
+
+    // var categorie = "";
+    // var afstand = "";
+    // var waardering = "";
+    // alert("ioN DE SETTINGS");
+    // db.transaction(function(trans)
+    // { 
+    //     alert("In de dbtans");
+    //     trans.executeSql('SELECT * FROM settings', [], function(trans, results)
+    //     {
+    //         alert("in de select statement");
+    //         var len = results.rows.length;
+    //         if(len>0)
+    //         {
+    //             categorie = results.rows.item(0).categorie;
+    //             afstand = results.rows.item(0).afstand;
+    //             waardering = results.rows.item(0).waardering;
+    //             succes(categorie, afstand, waardering);
+    //         }
+    //     }, 
+    //     function(err)
+    //     {
+    //         error(err);
+    //     });
+    // }, 
+    // function(err)
+    // {
+    //      error(err);
+    // });
 }
 
 
